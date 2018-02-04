1a50321d91fccee0741a00fe8412e83025fef36e
irql <interruptrequestlevel@gmail.com>
2018-02-04 11:55:16 -0500

    Added documentation for context.h

--- context.h
+++ context.h
@@ -1,4 +1,9 @@
+/** @file context.h
+ *  @brief Data structures that provide contexts to methods
+ */
+
+/** @brief The main (system) context */
 typedef struct main_context {
-    unsigned long system_page_size;
-    unsigned long system_phys_page_count;
+    unsigned long system_page_size;       ///< The result of a call made to sysconf(_SC_PAGE_SIZE)
+    unsigned long system_phys_page_count; ///< The result of a call made to sysconf(_SC_PHYS_PAGES)
 } Context_main;
