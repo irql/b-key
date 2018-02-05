ef04208a1436c6ff4c99e7d2dee13a06fd09d041
irql <interruptrequestlevel@gmail.com>
2018-02-04 22:08:50 -0500

    docs OCD continued

--- records.h
+++ records.h
@@ -2,8 +2,7 @@
  *  @brief Data structures (and macros) that comprise the database index
  */
 
-/** @brief Holds information relating to a bucket (\a key), including
- *         the number of pages allocated, as well as a pointer to
+/** @brief Holds information relating to a bucket (\a key), including the number of pages allocated, as well as a pointer to
  *         those pages in memory.
  *
  * This record has three composed values, which are encoded using the
@@ -14,11 +13,9 @@
  * - \a key - bucket
  * - \a page_count - Number of pages currently allocated
  * - \a offset - (\b disk-only) An offset to the start of the page region
- *
  */
 typedef struct ptbl_record {
-    /** @brief Holds the uppermost three bits of \a key and all
-     *         bits of \a page_count
+    /** @brief Holds the uppermost three bits of \a key and all bits of \a page_count
      *
      * | Range in bits | Size in bits | Description   |
      * | ------------- | -----------: | -----------   |
@@ -32,8 +29,7 @@ typedef struct ptbl_record {
      */
     unsigned int key_high_and_page_count;
 
-    /** @brief Holds the lowermost three bits of \a key and all
-     *         bits of \a offset
+    /** @brief Holds the lowermost three bits of \a key and all bits of \a offset
      *
      * | Range in bits | Size in bits | Description |
      * | ------------- | -----------: | ----------- |
@@ -48,33 +44,29 @@ typedef struct ptbl_record {
     unsigned int key_low_and_offset;
     
     /** @brief A pointer to the start of the allocated pages in memory
-     * @see kv_record.bucket_and_index
+     *  @see   kv_record.bucket_and_index
      */
     unsigned char *m_offset;
 
     unsigned int page_usage_length; ///< The length of \a page_usage in bytes
 
-    /** @brief Bookkeeping for usage status of all values across
-     *         all pages managed by this \a ptbl_record
-     *
-     * @see PTBL_CALC_PAGE_USAGE_BYTES()
-     * @see PTBL_CALC_PAGE_USAGE_BITS()
-     *
+    /** @brief Bookkeeping for usage status of all values across all pages managed by this \a ptbl_record
+     *  @see   PTBL_CALC_PAGE_USAGE_BYTES()
+     *  @see   PTBL_CALC_PAGE_USAGE_BITS()
      */
     unsigned char *page_usage;
 } Record_ptbl;
 
 /** @brief Calculate bytes used by multiple pages bookkeeping
  *
- * Computes the number of bytes it would take to represent the usage status of every value inside
- * \a y pages of bucket \a x
+ * Computes the number of bytes it would take to represent the usage status of every value inside \a y pages of bucket \a x
  *
- * @param x bucket
- * @param y number of pages
- * @returns number of bytes
- * @see PTBL_CALC_PAGE_USAGE_BYTES()
- * @see PTBL_CALC_PAGE_USAGE_BITS()
- * @see ptbl_record
+ * @param   x bucket
+ * @param   y number of pages
+ * @returns   number of bytes
+ * @see       PTBL_CALC_PAGE_USAGE_BYTES()
+ * @see       PTBL_CALC_PAGE_USAGE_BITS()
+ * @see       ptbl_record
  */
 #define PTBL_CALC_PAGE_USAGE_LENGTH(x,y) ((x <= 5) ?\
         (PTBL_CALC_PAGE_USAGE_BYTES(x) * y) :\
@@ -82,8 +74,7 @@ typedef struct ptbl_record {
 
 /** @brief Calculate bytes used by one pages bookkeeping
  *
- * Computes the number of bytes it would take to represent the
- * usage status of every value inside a page of bucket \a x
+ * Computes the number of bytes it would take to represent the usage status of every value inside a page of bucket \a x
  *
  * | Bucket | bytes |
  * | -----: | ----: |
@@ -95,18 +86,16 @@ typedef struct ptbl_record {
  * | 6      | ^     |
  * | \a x   | ^     |
  *
- * @param x bucket \f$0 \leq x \leq 63\f$
- * @returns number of bytes
- * @see PTBL_CALC_PAGE_USAGE_BITS()
- * @see ptbl_record
- *
+ * @param   x bucket \f$0 \leq x \leq 63\f$
+ * @returns   number of bytes
+ * @see       PTBL_CALC_PAGE_USAGE_BITS()
+ * @see       ptbl_record
  */
 #define PTBL_CALC_PAGE_USAGE_BYTES(x) ((x < 5) ? (32 >> x) : 1)
 
 /** @brief Calculate bits used by one page's bookkeeping
  *
- * Computes the number of bits it would take to represent the
- * usage status of every value inside a page of bucket * \a x
+ * Computes the number of bits it would take to represent the usage status of every value inside a page of bucket \a x
  *
  * | Bucket | bits |
  * | -----: | ----: |
@@ -118,18 +107,16 @@ typedef struct ptbl_record {
  * | 9      | ^     |
  * | \a x   | ^     |
  *
- * @param x bucket \f$0 \leq x \leq 63\f$
+ * @param   x bucket \f$0 \leq x \leq 63\f$
  * @returns number of bits
- * @see PTBL_CALC_PAGE_USAGE_BYTES()
- * @see ptbl_record
- *
+ * @see     PTBL_CALC_PAGE_USAGE_BYTES()
+ * @see     ptbl_record
  */
 #define PTBL_CALC_PAGE_USAGE_BITS(x) ((x < 8) ? (256 >> x) : 1)
 
 /** @brief Calculate max value size for bucket \a x
  *
- * Computes the maximum number of bytes that a value in
- * a page in bucket \a x can occupy
+ * Computes the maximum number of bytes that a value in a page in bucket \a x can occupy
  *
  * | Bucket | word size |
  * | -----: | --------: |
@@ -140,11 +127,10 @@ typedef struct ptbl_record {
  * | \a x   | (1 << (4 + \a x)) |
  * | 63     | 147EB     |
  *
- * @param x bucket \f$0 \leq x \leq 63\f$
- * @returns max value length in bytes
- * @see ptbl_record
- * @see kv_record.bucket_and_index
- *
+ * @param   x bucket \f$0 \leq x \leq 63\f$
+ * @returns   max value length in bytes
+ * @see       ptbl_record
+ * @see       kv_record.bucket_and_index
  */
 #define PTBL_CALC_BUCKET_WORD_SIZE(x) (1 << (4 + x))
 
@@ -152,53 +138,37 @@ typedef struct ptbl_record {
 #define PTBL_KEY_HIGH_BITMASK 0x38 ///< Upper three bits
 #define PTBL_KEY_LOW_BITMASK 0x7 ///< Lower three bits
 
-/** @brief Amount to shift the bits in ptbl_record.key_high_and_page_count
- *  right by to get the uppermost three bits of \a key
- */
+/** @brief Amount to shift the bits in ptbl_record.key_high_and_page_count right by to get the uppermost three bits of \a key */
 #define PTBL_KEY_HIGH_SHIFT 26
 
-/** @brief Amount to shift the bits in ptbl_record.key_low_and_offset
- *  right by to get the lowermost three bits of \a key
- */
+/** @brief Amount to shift the bits in ptbl_record.key_low_and_offset right by to get the lowermost three bits of \a key */
 #define PTBL_KEY_LOW_SHIFT 29
 
-/** @brief Get \a page_count from a ptbl_record
- *
- * @param x ptbl_record (\b not a pointer)
- *
- * @see ptbl_record.key_high_and_page_count
- *
+/** @brief   Get \a page_count from a ptbl_record
+ *  @param x ptbl_record (\b not a pointer)
+ *  @see     ptbl_record.key_high_and_page_count
  */
 #define PTBL_RECORD_GET_PAGE_COUNT(x) (x.key_high_and_page_count & ~PTBL_KEY_BITMASK)
 
-/** @brief Set \a page_count in a ptbl_record
- *
- * @param x ptbl_record (\b not a pointer)
- * @param y new \a page_count (unsigned long)
- *
- * @see ptbl_record.key_high_and_page_count
- *
+/** @brief   Set \a page_count in a ptbl_record
+ *  @param x ptbl_record (\b not a pointer)
+ *  @param y new \a page_count (unsigned long)
+ *  @see     ptbl_record.key_high_and_page_count
  */
 #define PTBL_RECORD_SET_PAGE_COUNT(x,y) \
     x.key_high_and_page_count &= PTBL_KEY_BITMASK; \
     x.key_high_and_page_count |= ((unsigned int)y & ~PTBL_KEY_BITMASK);
 
-/** @brief Get \a offset from a ptbl_record
- *
- * @param x ptbl_record (\b not a pointer)
- *
- * @see ptbl_record.key_low_and_offset
- *
+/** @brief   Get \a offset from a ptbl_record
+ *  @param x ptbl_record (\b not a pointer)
+ *  @see     ptbl_record.key_low_and_offset
  */
 #define PTBL_RECORD_GET_OFFSET(x) (x.key_low_and_offset & ~PTBL_KEY_BITMASK)
 
-/** @brief Set \a offset in a ptbl_record
- *
- * @param x ptbl_record (\b not a pointer)
- * @param y new \a offset (unsigned int)
- *
- * @see ptbl_record.key_low_and_offset
- *
+/** @brief   Set \a offset in a ptbl_record
+ *  @param x ptbl_record (\b not a pointer)
+ *  @param y new \a offset (unsigned int)
+ *  @see     ptbl_record.key_low_and_offset
  */
 #define PTBL_RECORD_SET_OFFSET(x,y) \
     x.key_low_and_offset &= PTBL_KEY_BITMASK; \
@@ -209,10 +179,8 @@ typedef struct ptbl_record {
  * Note that \a key is the bucket number.
  *
  * @param x ptbl_record (\b not a pointer)
- *
- * @see ptbl_record.key_high_and_page_count
- * @see ptbl_record.key_low_and_offset
- *
+ * @see     ptbl_record.key_high_and_page_count
+ * @see     ptbl_record.key_low_and_offset
  */
 #define PTBL_RECORD_GET_KEY(x) (((x.key_high_and_page_count & PTBL_KEY_BITMASK) >> PTBL_KEY_HIGH_SHIFT) | ((x.key_low_and_offset & PTBL_KEY_BITMASK) >> PTBL_KEY_LOW_SHIFT))
 
@@ -225,10 +193,8 @@ typedef struct ptbl_record {
  *
  * @param x ptbl_record (\b not a pointer)
  * @param y bucket \f$0 \leq y \leq 63\f$
- *
- * @see ptbl_record.key_high_and_page_count
- * @see ptbl_record.key_low_and_offset
- *
+ * @see     ptbl_record.key_high_and_page_count
+ * @see     ptbl_record.key_low_and_offset
  */
 #define PTBL_RECORD_SET_KEY(x,y) \
     x.key_high_and_page_count &= ~PTBL_KEY_BITMASK; \
@@ -236,13 +202,10 @@ typedef struct ptbl_record {
     x.key_high_and_page_count |= ((y & PTBL_KEY_HIGH_BITMASK) << PTBL_KEY_HIGH_SHIFT); \
     x.key_low_and_offset |= ((y & PTBL_KEY_LOW_BITMASK) << PTBL_KEY_LOW_SHIFT);
 
-/** @brief Holds information for a key/value pair, including the
- *         bucket the value resides in, it's \a index (offset) into
- *         the bucket (ptbl_record.m_offset + \a index), in
- *         addition to the size of the value in bytes
+/** @brief Holds information for a key/value pair, including the bucket the value resides in, it's \a index (offset) into the
+ *         bucket (ptbl_record.m_offset + \a index), in addition to the size of the value in bytes
  *
- * This record has four composed values, which are encoded using the
- * bits of \a flags_and_size and \a bucket_and_index and require a
+ * This record has four composed values, which are encoded using the bits of \a flags_and_size and \a bucket_and_index and require a
  * macro to either get or set them.
  *
  * The composed values are:
@@ -251,7 +214,6 @@ typedef struct ptbl_record {
  * - \a bucket - Which bucket (ptbl_record) the page that holds this value resides in
  * - \a index - Used to determine the offset into the pages of bucket that this
  *              value starts at (value = ptbl_record.m_offset + index * PTBL_CALC_BUCKET_WORD_SIZE(bucket))
- *
  */
 typedef struct kv_record {
     /** @brief Holds the bits of both \a flags and \a size
@@ -292,43 +254,31 @@ typedef struct kv_record {
 #define KV_RECORD_BUCKET_SHIFT 58 ///< Amount to shift \a bucket_and_index right by to extract \a bucket
 #define KV_RECORD_BUCKET_BITMASK ((unsigned long)0x3F << KV_RECORD_BUCKET_SHIFT) ///< To select the upper six bits
 
-/** @brief Get \a bucket from a kv_record
- *
- * @param x kv_record (\b not a pointer)
- *
- * @see kv_record.bucket_and_index
- *
+/** @brief   Get \a bucket from a kv_record
+ *  @param x kv_record (\b not a pointer)
+ *  @see     kv_record.bucket_and_index
  */
 #define KV_RECORD_GET_BUCKET(x) ((x.bucket_and_index & KV_RECORD_BUCKET_BITMASK) >> KV_RECORD_BUCKET_SHIFT)
 
-/** @brief Set \a bucket in a kv_record
- *
- * @param x kv_record (\b not a pointer)
- * @param y bucket \f$0 \leq y \leq 63\f$
- *
- * @see kv_record.bucket_and_index
- *
+/** @brief   Set \a bucket in a kv_record
+ *  @param x kv_record (\b not a pointer)
+ *  @param y bucket \f$0 \leq y \leq 63\f$
+ *  @see     kv_record.bucket_and_index
  */
 #define KV_RECORD_SET_BUCKET(x,y) \
     x.bucket_and_index &= ~KV_RECORD_BUCKET_BITMASK; \
     x.bucket_and_index |= ((unsigned long)(y & (KV_RECORD_BUCKET_BITMASK >> KV_RECORD_BUCKET_SHIFT)) << KV_RECORD_BUCKET_SHIFT);
 
-/** @brief Get \a index from a kv_record
- *
- * @param x kv_record (\b not a pointer)
- *
- * @see kv_record.bucket_and_index
- *
+/** @brief   Get \a index from a kv_record
+ *  @param x kv_record (\b not a pointer)
+ *  @see     kv_record.bucket_and_index
  */
 #define KV_RECORD_GET_INDEX(x) (x.bucket_and_index & ~KV_RECORD_BUCKET_BITMASK)
 
-/** @brief Set \a index in a kv_record
- *
- * @param x kv_record (\b not a pointer)
- * @param y index
- *
- * @see kv_record.bucket_and_index
- *
+/** @brief   Set \a index in a kv_record
+ *  @param x kv_record (\b not a pointer)
+ *  @param y index
+ *  @see     kv_record.bucket_and_index
  */
 #define KV_RECORD_SET_INDEX(x,y) \
     x.bucket_and_index &= KV_RECORD_BUCKET_BITMASK; \
@@ -337,44 +287,32 @@ typedef struct kv_record {
 #define KV_RECORD_FLAGS_SHIFT 56 ///< Amount to shift \a flags_and_size right by to extract \a flags
 #define KV_RECORD_FLAGS_BITMASK ((unsigned long)0xFF << KV_RECORD_FLAGS_SHIFT) ///< To select the upper 8 bits
 
-/** @brief Get \a size from a kv_record
- *
- * @param x kv_record (\b not a pointer)
- * @returns Size of the value in bytes
- *
- * @see kv_record.flags_and_size
- *
+/** @brief     Get \a size from a kv_record
+ *  @param   x kv_record (\b not a pointer)
+ *  @returns   Size of the value in bytes
+ *  @see       kv_record.flags_and_size
  */
 #define KV_RECORD_GET_SIZE(x) (x.flags_and_size & ~KV_RECORD_FLAGS_BITMASK)
 
-/** @brief Set \a size in a kv_record
- *
- * @param x kv_record (\b not a pointer)
- * @param y size
- *
- * @see kv_record.flags_and_size
- *
+/** @brief   Set \a size in a kv_record
+ *  @param x kv_record (\b not a pointer)
+ *  @param y size
+ *  @see     kv_record.flags_and_size
  */
 #define KV_RECORD_SET_SIZE(x,y) \
     x.flags_and_size &= KV_RECORD_FLAGS_BITMASK; \
     x.flags_and_size |= (y & ~KV_RECORD_FLAGS_BITMASK);
 
-/** @brief Get \a flags in a kv_record
- *
- * @param x kv_record (\b not a pointer)
- *
- * @see kv_record.flags_and_size
- *
+/** @brief   Get \a flags in a kv_record
+ *  @param x kv_record (\b not a pointer)
+ *  @see     kv_record.flags_and_size
  */
 #define KV_RECORD_GET_FLAGS(x) ((x.flags_and_size & KV_RECORD_FLAGS_BITMASK) >> KV_RECORD_FLAGS_SHIFT)
 
-/** @brief Set \a flags in a kv_record
- *
- * @param x kv_record (\b not a pointer)
- * @param y flags
- *
- * @see kv_record.flags_and_size
- *
+/** @brief   Set \a flags in a kv_record
+ *  @param x kv_record (\b not a pointer)
+ *  @param y flags
+ *  @see     kv_record.flags_and_size
  */
 #define KV_RECORD_SET_FLAGS(x,y) \
     x.flags_and_size &= ~KV_RECORD_FLAGS_BITMASK; \
@@ -390,25 +328,20 @@ typedef struct database_record {
 
 /** @brief Helper to instantiate a new record type
  *
- * This will create a new variable, in addition to
- * allocating space for it.
+ * This will create a new variable, in addition to allocating space for it.
  *
  * @param x Record type
  * @param y Variable name
- *
  */
 #define RECORD_CREATE(x,y) \
     x *y = (x *)memory_alloc(sizeof(x))
 
 /** @brief Helper to allocate a new record type
  *
- * This will only allocate \a sizeof(x) bytes, and
- * assign it to \a y. It does not create a new
- * variable named \a y.
+ * This will only allocate \a sizeof(x) bytes, and assign it to \a y. It does not create a new variable named \a y.
  *
  * @param x Record type
  * @param y Variable name
- *
  */
 #define RECORD_ALLOC(x, y) \
     y = (x *)memory_alloc(sizeof(x))
