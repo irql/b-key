f61b3dd872d1d9f1fae83bfeac365a9b09ed95bd
irql <interruptrequestlevel@gmail.com>
2018-02-13 17:29:04 -0500

    documentation-only branch

--- os.h
+++
@@ -1,7 +0,0 @@
-/** @file os.h
- *  @brief Defines used to identify which OS should be compiled for
- */
-
-#if defined(__MACOSX__) || defined(__APPLE__) || defined(__OpenBSD__)
-#define __MACOSX__
-#endif
